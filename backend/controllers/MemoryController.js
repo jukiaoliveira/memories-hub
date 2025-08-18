12ac5aed052bc7804a8e13191e20330ce9c2c8c5
jukiaoliveira <jurafaoliveira@icloud.com>
2025-08-18 18:44:42 -0300

    add comentários às memórias

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -3,14 +3,14 @@ const Memory = require("../models/Memory");
 const fs = require("fs");
 
 const removeOldImage = (memory) => {
-    fs.unlink(`public/${memory.src}`, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Imagem excluída do servidor!");
-        }
-    });
-}
+  fs.unlink(`public/${memory.src}`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Imagem excluída do servidor!");
+    }
+  });
+};
 
 const createMemory = async (req, res) => {
   try {
@@ -33,7 +33,8 @@ const createMemory = async (req, res) => {
     await newMemory.save();
 
     res.json({
-      msg: "Memória criada com sucesso!", newMemory
+      msg: "Memória criada com sucesso!",
+      newMemory,
     });
   } catch (error) {
     console.log(error.message);
@@ -42,102 +43,128 @@ const createMemory = async (req, res) => {
 };
 
 const getMemories = async (req, res) => {
-    try {
-        const memories = await Memory.find();
+  try {
+    const memories = await Memory.find();
 
-        res.json(memories);
-    } catch (error) {
-        res.status(500).send("Ocorreu um erro!");
-    }
-}
+    res.json(memories);
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
 
 const getMemory = async (req, res) => {
-    try {
-        const memory = await Memory.findById(req.params.id);
+  try {
+    const memory = await Memory.findById(req.params.id);
 
-        if(!memory) {
-            return res.status(404).json({ msg: "Memória não encontrada!"});
-        }
-
-        res.json(memory);
-    } catch (error) {
-        res.status(500).send("Ocorreu um erro!");
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
     }
-}
+
+    res.json(memory);
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
 
 const deleteMemory = async (req, res) => {
-    try {
+  try {
+    const memory = await Memory.findByIdAndDelete(req.params.id);
 
-        const memory = await Memory.findByIdAndDelete(req.params.id)
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
 
-        if(!memory) {
-            return res.status(404).json({ msg: "Memória não encontrada!"});
-        }
+    removeOldImage(memory);
 
-        removeOldImage(memory);
-
-        res.json({ msg: "Memória deletada com sucesso!" });
-    } catch (error) {
-        res.status(500).send("Ocorreu um erro!");
-    }   
-}
+    res.json({ msg: "Memória deletada com sucesso!" });
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
 
 const updateMemory = async (req, res) => {
-    try {
-        const  { title, description } = req.body;
+  try {
+    const { title, description } = req.body;
 
-        let src = null
+    let src = null;
 
-        if(req.file) {
-            src = `images/${req.file.filename}`;
-        }
-
-        const memory = await Memory.findById(req.params.id)
-
-        if(!memory) {
-            return res.status(404).json({ msg: "Memória não encontrada!"});
-        }
-
-        if(src) {
-            removeOldImage(memory)
-        }
-
-        const updateData = {};
-
-        if(title) updateData.title = title;
-        if (description) updateData.description = description;
-        if (src) updateData.src = src;
-
-        const updateMemory = await Memory.findByIdAndUpdate(
-            req.params.id,
-            updateData,
-            { new: true }   
-        )
-
-        res.json({updateMemory, msg: "Memória atualizada com sucesso!"});
-    } catch (error) {
-        res.status(500).send("Ocorreu um erro!");
+    if (req.file) {
+      src = `images/${req.file.filename}`;
     }
-}
 
-const toggleFavorite = async(req, res) => {
-    try {
+    const memory = await Memory.findById(req.params.id);
 
-        const memory = await Memory.findById(req.params.id)
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
 
-        if(!memory) {
-            return res.status(404).json({ msg: "Memória não encontrada!"});
-        }
+    if (src) {
+      removeOldImage(memory);
+    }
 
-        memory.favorite = !memory.favorite;
+    const updateData = {};
 
-        await memory.save()
+    if (title) updateData.title = title;
+    if (description) updateData.description = description;
+    if (src) updateData.src = src;
 
-        res.json({ msg: "Adicionada aos favoritos!", memory });
-    } catch (error) {
-        res.status(500).send("Ocorreu um erro!");
-    }  
-}
+    const updateMemory = await Memory.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true }
+    );
+
+    res.json({ updateMemory, msg: "Memória atualizada com sucesso!" });
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
+
+const toggleFavorite = async (req, res) => {
+  try {
+    const memory = await Memory.findById(req.params.id);
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
+
+    memory.favorite = !memory.favorite;
+
+    await memory.save();
+
+    res.json({ msg: "Adicionada aos favoritos!", memory });
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const { name, text } = req.body;
+
+    if (!name || !text) {
+      return res
+        .status(400)
+        .json({ msg: "Por favor, preencha todos os campos." });
+    }
+
+    const comment = { name, text }
+
+    const memory = await Memory.findById(req.params.id);
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
+
+    memory.comments.push = (comment);
+
+    await memory.save();
+
+    res.json({ msg: "Comentário adicionado!", memory });
+  } catch (error) {
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
 
 module.exports = {
   createMemory,
@@ -146,4 +173,5 @@ module.exports = {
   deleteMemory,
   updateMemory,
   toggleFavorite,
+  addComment,
 };
