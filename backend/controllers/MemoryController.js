7e23ec91ff869fe34be1c266c329b4c3d3152061
jukiaoliveira <jurafaoliveira@icloud.com>
2025-08-18 18:36:06 -0300

    favoritando memória

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -120,10 +120,30 @@ const updateMemory = async (req, res) => {
     }
 }
 
+const toggleFavorite = async(req, res) => {
+    try {
+
+        const memory = await Memory.findById(req.params.id)
+
+        if(!memory) {
+            return res.status(404).json({ msg: "Memória não encontrada!"});
+        }
+
+        memory.favorite = !memory.favorite;
+
+        await memory.save()
+
+        res.json({ msg: "Adicionada aos favoritos!", memory });
+    } catch (error) {
+        res.status(500).send("Ocorreu um erro!");
+    }  
+}
+
 module.exports = {
   createMemory,
   getMemories,
   getMemory,
   deleteMemory,
-  updateMemory
+  updateMemory,
+  toggleFavorite,
 };
