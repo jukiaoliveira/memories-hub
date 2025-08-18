88ea0d21b1d4167bbee4cd372098545a6e19f7fc
jukiaoliveira <jurafaoliveira@icloud.com>
2025-08-18 18:28:27 -0300

    atualização de memória

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -82,9 +82,48 @@ const deleteMemory = async (req, res) => {
     }   
 }
 
+const updateMemory = async (req, res) => {
+    try {
+        const  { title, description } = req.body;
+
+        let src = null
+
+        if(req.file) {
+            src = `images/${req.file.filename}`;
+        }
+
+        const memory = await Memory.findById(req.params.id)
+
+        if(!memory) {
+            return res.status(404).json({ msg: "Memória não encontrada!"});
+        }
+
+        if(src) {
+            removeOldImage(memory)
+        }
+
+        const updateData = {};
+
+        if(title) updateData.title = title;
+        if (description) updateData.description = description;
+        if (src) updateData.src = src;
+
+        const updateMemory = await Memory.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            { new: true }   
+        )
+
+        res.json({updateMemory, msg: "Memória atualizada com sucesso!"});
+    } catch (error) {
+        res.status(500).send("Ocorreu um erro!");
+    }
+}
+
 module.exports = {
   createMemory,
   getMemories,
   getMemory,
-  deleteMemory
+  deleteMemory,
+  updateMemory
 };
