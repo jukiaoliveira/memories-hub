1d5e174fa985936c592c9bc499788040f11af988
jukiaoliveira <jurafaoliveira@icloud.com>
2025-09-13 18:33:48 -0300

    criando barra de navegação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 
 import { Outlet } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 function App() {
   return (
       <div className="App">
-        <h1>Memories</h1>
+        <Navbar />
         <div className="container">
           <Outlet />
         </div>
+        <Footer />
       </div>
         
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return <nav>
+        <h2>
+            <Link to="/">Memories</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/add-memory">Adiconar Memória</Link>
+            </li>
+        </ul>
+    </nav>;
+};
+
+export default Navbar;
