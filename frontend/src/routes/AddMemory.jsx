17d976870756eaa43c59281515f5ff25a9ae41e1
jukiaoliveira <jurafaoliveira@icloud.com>
2025-09-13 19:27:31 -0300

    criação do formulário de memória

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import axios from "axios";
+
+import { useState } from "react";
+
+import "./AddMemory.css";
 
 const AddMemory = () => {
-    return <div>Add Memory</div>
+  return (
+    <div className="add-memory-page">
+      <h2>Crie uma nova memória</h2>
+      <form>
+        <label>
+          <p>Título:</p>
+          <input type="text" placeholder="Defina um título" name="title" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea
+            placeholder="Explique o que aconteceu..."
+            name="description"
+          ></textarea>
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" value="Adicionar" className="btn" />
+      </form>
+    </div>
+  );
 };
 
 export default AddMemory;
