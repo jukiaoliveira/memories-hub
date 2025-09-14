9312998acd9b39e7997bf32ccd1db0813f376db4
jukiaoliveira <jurafaoliveira@icloud.com>
2025-09-13 21:20:36 -0300

    estilizando a Home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,30 +1,44 @@
-import axios from '../axios-config'
+import axios from "../axios-config";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 const Home = () => {
-    const [memories, setMemories] = useState([]);
+  const [memories, setMemories] = useState([]);
 
-    useEffect(() => {
-        const getMemories = async () => {
-            const res = await axios.get('/memories');
+  useEffect(() => {
+    const getMemories = async () => {
+      const res = await axios.get("/memories");
 
-            setMemories(res.data);
-        };
+      setMemories(res.data);
+    };
 
-        getMemories();
-    }, []);
+    getMemories();
+  }, []);
 
-    return <div className='home'>
-        <h2>Confira as últimas Memórias</h2>
-        <div className='memories-container'>
-            {memories.length > 0 && memories.map((memory) => (
-                <p>{memory.title}</p>
-            ))}
-        </div>
+  return (
+    <div className="home">
+      <h2>Confira as últimas Memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 &&
+          memories.map((memory) => (
+            <div className="memory" key={memory._id}>
+              <img
+                src={`${axios.defaults.baseURL}/${memory.src}`}
+                alt={memory.title}
+              />
+              <p>{memory.title}</p>
+              <Link className="btn" to={`/memories/${memory._id}`}>
+                Comentar
+              </Link>
+            </div>
+          ))}
+      </div>
     </div>
+  );
 };
 
 export default Home;
