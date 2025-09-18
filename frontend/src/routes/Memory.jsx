85fbc9aa50b242da53d043224b1adf41b8ac20ac
jukiaoliveira <jurafaoliveira@icloud.com>
2025-09-17 21:46:55 -0300

    criando função de enviar comentários

--- frontend/src/routes/Memory.jsx
+++ frontend/src/routes/Memory.jsx
@@ -6,13 +6,16 @@ import { useParams } from "react-router-dom";
 
 import { toast } from "react-toastify";
 
-import './Memory.css';
+import "./Memory.css";
 
 const Memory = () => {
   const { id } = useParams();
 
   const [memory, setMemory] = useState(null);
   const [comments, setComments] = useState([]);
+
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
 
   useEffect(() => {
     const getMemory = async () => {
@@ -26,6 +29,30 @@ const Memory = () => {
     getMemory();
   }, [id]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const comment = { name, text }
+
+      const res = await axios.patch(`/memories/${memory._id}/comment`, comment);
+
+      const lastComment = res.data.memory.comments.pop()
+
+      setComments([...comments, lastComment]);
+
+      setName("");
+      setText("");
+
+      toast.success(res.data.msg)
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.response.data.msg);
+  }
+  };
+
   if (!memory) return <p>Carregando...</p>;
 
   return (
@@ -35,12 +62,21 @@ const Memory = () => {
       <p>{memory.description}</p>
       <div className="comment-form">
         <h3>Envie o seu comentário:</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
-            <input type="text" placeholder="Escreva o seu nome" />
+            <input
+              type="text"
+              placeholder="Escreva o seu nome"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
           </label>
           <label>
-            <textarea placeholder="Deixe o seu comentário"></textarea>
+            <textarea
+              placeholder="Deixe o seu comentário"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            ></textarea>
           </label>
           <input type="submit" value="Enviar" className="btn" />
         </form>
